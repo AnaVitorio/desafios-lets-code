f2fe960c645a61c606639b6e46dc34bc91d631ed
AnaVitorio <65178482+AnaVitorio@users.noreply.github.com>
2022-06-26 19:32:51 -0300

    Add files via upload
    Exercício sobre generics

---
+++ Generics/MyOwnLists.java
@@ -0,0 +1,12 @@
+package lets.code.Generics;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class MyOwnLists{
+    public static <T> List<T> asListedSorted(List<T> lista, Comparator<T> comparator){
+        Collections.sort(lista, comparator);
+        return lista;
+    }
+}
