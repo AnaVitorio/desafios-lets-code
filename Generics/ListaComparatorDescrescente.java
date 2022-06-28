bc0bfd34dfb5e7f3ece2d4bde778f8016b1d1797
AnaVitorio <65178482+AnaVitorio@users.noreply.github.com>
2022-06-27 21:03:48 -0300

    Add files via upload

---
+++ Generics/ListaComparatorDescrescente.java
@@ -0,0 +1,36 @@
+package lets.code.Generics;
+
+import java.util.Comparator;
+
+public class ListaComparatorDescrescente<Generica> implements Comparator<Generica>  {
+
+    @Override
+    public int compare(Generica arg1, Generica arg2) {
+
+        if((arg1 instanceof Nomes) && (arg2 instanceof Nomes)){
+            Nomes nome1 = (Nomes) arg1;
+            Nomes nome2 = (Nomes) arg2;
+
+            return nome2.getNome().compareTo(nome1.getNome());
+        } 
+
+        if((arg1 instanceof Idades) && (arg2 instanceof Idades)){
+            Idades idade1 = (Idades) arg1;
+            Idades idade2 = (Idades) arg2;
+            
+            if(idade1.getIdade() < idade2.getIdade()){
+                return 1;
+            }
+
+            if(idade1.getIdade() > idade2.getIdade()){
+                return -1;
+            }
+
+
+        }
+
+        return 0;
+
+    }
+    
+}
